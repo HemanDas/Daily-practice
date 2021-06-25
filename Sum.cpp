f9ab60e09cbafedc4902e476e45a3ecc5bbc1d8f
HemanDas <hemandas630@gmail.com>
2021-06-25 14:43:57 +0545

    Sum using class and array

--- Sum.cpp
+++ Sum.cpp
@@ -2,22 +2,29 @@
 using namespace std;
 class Sum
 {
+    
     public:
-        int a[5];
-        Sum()
+        int n;
+        Sum getterms(){
+        cout<<"Enter the no of terms that needs to be added"<<endl;
+        cin>>n;
+        }
+        Sum getnum()
         {
+            int a[n];
             int i;
             cout<<"Enter five numbers:"<<endl;
-            for(i=0;i<5;i++)
+            for(i=0;i<n;i++)
             {
                 cin>>a[i];
             }    
         }
         int add()
         {
+            int a[n];
             int i;
             int sum=0;
-            for (i=0;i<5;i++)
+            for (i=0;i<n;i++)
             {
                 sum=sum+a[i];
             }
@@ -28,6 +35,8 @@ class Sum
             
 int main()
 {
-    Sum sum;
-    sum.add();
+    Sum s1;
+    s1.getterms();
+    s1.getnum();
+    s1.add();
 }
